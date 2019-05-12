e6ad906577dce01703115a083c0cb277099b20f8
Radovan <Radovan1993@live.com>
2019-05-12 11:52:38 +0200

    EmailSender change

--- src/exercises/EmailSender.java
+++ src/exercises/EmailSender.java
@@ -2,20 +2,32 @@ package exercises;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class EmailSender {
 
 	void go() {
-		/*
-		 * Use the methods below to send an email and print a warning if the email did
-		 * not send correctly.
-		 */
 
-		
+		String email = JOptionPane.showInputDialog("Enter email address of the recipient.");
+		setEmailAddress(email);
+		String message = JOptionPane.showInputDialog("Enter message:");
+		if (send(message) && isValid(email))
+			JOptionPane.showMessageDialog(null, "Email has been sent");
+		else
+			JOptionPane.showMessageDialog(null, "The email has not been sent correctly");
+
 	}
 
-	/* Returns true if email was sent successfully, false if it failed to send. */
+	boolean isValid(String email) {
+		if (!(this.emailAddress.contains("@") && this.emailAddress.contains("."))) {
+			System.err.println("Invalid email address.");
+			return false;
+		}
+		return true;
+	}
+
 	boolean send(String greeting) {
-		if (this.emailAddress.isBlank()) {
+		if (this.emailAddress.isEmpty()) {
 			System.err.println("You did not provide a recipient for the email!");
 			return false;
 		}
